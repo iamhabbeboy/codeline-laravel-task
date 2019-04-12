95252e47947df972b26eb07b7e3412a54924087c
Azeez Abiodun <iamhabbeboy@gmail.com>
2019-04-12 18:02:09 +0100

    initial commit
    database schema and migration

---
+++ app/Http/Controllers/FilmController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function show()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function single()
+    {
+
+    }
+    
+}
---
+++ database/migrations/2019_04_12_163418_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('name');
+            $table->text('description');
+            $table->string('release_date');
+            $table->integer('rating');
+            $table->double('ticket_price');
+            $table->string('country');
+            $table->string('genre');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
